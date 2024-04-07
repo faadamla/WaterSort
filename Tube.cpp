d4e0dc4f1fafe5da1e6a826deb0654f2d61b402d
Adam L Farkas <faadamla@gmail.com>
2024-04-07 22:22:54 +0200

    Refactoring tube.cpp

--- Tube.cpp
+++ Tube.cpp
@@ -7,15 +7,19 @@ using uc = unsigned char;
 template<int N>
 class Tube{
 private:
-	std::array<uc, N> elements;
 	void update() {
 		top_color = elements[0];
+		top_color_depth = 0;
 		top_zeros = 0;
 		correct = true;
 		bool had_zero = false;
 		for (auto& e : elements) {
 			if (e > 0) {
-				top_color = e;
+				if (top_color == e) { top_color_depth++; }
+				else {
+					top_color_depth = 1;
+					top_color = e;
+				}
 				top_zeros = 0;
 				if (had_zero) correct = false;
 			}
@@ -27,11 +31,13 @@ private:
 	}
 
 public:
+	std::array<uc, N> elements; //Zero is the bottom of the tube
 	uc size;
 	uc top_color;
 	uc top_zeros;
+	uc top_color_depth;
 	bool correct;
-	Tube(std::array<uc, N> elements): elements(elements), size(elements.size()) {
+	Tube(std::array<uc, N> elements): elements(elements), size(N) {
 		this->update();
 	}
 	void Describe() {
@@ -43,5 +49,52 @@ public:
 		printf("Size = %d\n", size);
 		printf("top_color = %d\n", top_color);
 		printf("top_zeros = %d\n", top_zeros);
+		printf("top_color_depth = %d\n", top_color_depth);
+		printf("correct: %s\n\n", correct ? "true" : "false");
+	}
+	void print() {
+		for (int i = 0; i < elements.size(); i++) {
+			printf("%d", elements[i]);
+			if (i < elements.size() - 1) {
+				printf(", ");
+			}
+		}
+		std::cout << std::endl;
+	}
+	bool can_pour_to(Tube<N> &to_tube) {
+		/*
+		One can pour, if
+		-top_color of to_tube is equal from_tube's top_color or to_tube is empty
+		and
+		-There are top zeros in to_tube
+		and
+		-from_tube is not empty
+		*/
+		if (this == &to_tube) {
+			return false; }
+		if (((to_tube.top_color == 0) || (to_tube.top_color == top_color)) && to_tube.top_zeros > 0 && top_color > 0) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	void try_pour(Tube<N> &to_tube) {
+		if (this->can_pour_to(to_tube)) {
+			auto pour_color = top_color;
+			uc pour_length = std::min(to_tube.top_zeros, top_color_depth);
+			for (uc i = 0; i < pour_length; i++) {
+				elements[size-top_zeros- pour_length +i] = 0;
+				to_tube.elements[to_tube.size-to_tube.top_zeros+i] = pour_color;
+				//std::cout << "Modified inex: " << to_tube.size - to_tube.top_zeros + i << std::endl;	
+			}
+			this->update();
+			to_tube.update();
+
+		}
+		else {
+			std::cout << "Pour is not possible." << std::endl;
+			return;
+		}
 	}
 };
--- WaterSort.cpp
+++ WaterSort.cpp
@@ -8,12 +8,23 @@ using namespace std;
 
 int main()
 {
-	cout << "Hello CMake." << std::endl;
+	//cout << "Hello CMake." << std::endl;
 
 	Tube<4> t1({ 0, 1, 2, 3 });
-	Tube<4> t2({ 3,3,0,0 });
+	Tube<4> t2({ 1,3,3,0 });
+	Tube<4> t3({ 1,2,3,0 });
 
-	t1.Describe();
-	t2.Describe();
+	//t1.Describe();
+	cout << "Before pour:" << endl;
+	t2.print();
+	t3.print();
+
+	t2.try_pour(t2);
+	cout << "After pour:" << endl;
+	t2.print();
+	t3.print();
+
+	//std:array<int, 3> a{ 0,1, 2 };
+	//std:array<int, 3> b{ a, 42 };
 	return 0;
 }
