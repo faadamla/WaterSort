16cd34fababc7c77fa529813ae691691020f00ce
faadamla <faadamla@gmail.com>
2024-04-17 11:18:30 +0200

    Factorial implementation

--- Game.cpp
+++ Game.cpp
@@ -4,3 +4,14 @@ template<uc NumberOfTubes, uc DepthOfTube>
 std::vector<State<NumberOfTubes,  DepthOfTube>> allInitialState(uc ncolors, uc empty=2){
 
 }
+
+long long int factorial(unsigned int n){
+    if(n<=1){return 1;}
+    else{
+        long long int x=1;
+        for(unsigned int i=2;i<=n; i++){
+            x *= i;
+        }
+        return x;
+    }
+}
--- WaterSort.cpp
+++ WaterSort.cpp
@@ -50,10 +50,10 @@ int main()
 	my_type_test.print();
 	cout << "my_type: " << my_type_test.my_type();
 
-	cout << endl << "===================Comparison test:=================" << endl;
+	cout << endl << "===================Tube Comparison test:=================" << endl;
 	cout << "Compare: " << (Tube<4>({ 1, 1, 1, 1 }) < Tube<4>({ 1,2,3,4 }));
 
-	cout << endl << "\n\n===================Game::possibleMoves test:=================" << endl;
+	cout << endl << "\n\n===================State::possibleMoves test:=================" << endl;
 	State<4, 4> TestGame1({ 1,1,2,0,1,1,2,0,2,2,0,0,3,3,3,3 });
 	TestGame1.print();
 	auto&& moves = TestGame1.possibleMoves();
@@ -61,17 +61,7 @@ int main()
 		printf("%d -> %d\n", m.first, m.second);
 	}
 
-	cout << endl << "\n\n===================Game::discoverAllStatesFromInitialState test:=================" << endl;
-	const State<4, 4> TestGame2({ 1,2,1,2,1,3,3,2,1,2,3,3,0,0,0,0 });
-	TestGame2.print();
-	auto && allDiscovered = discoverAllStatesFromInitialState(TestGame2);
-	for (auto&& s : allDiscovered) {
-		// s.print();
-		// cout<<endl;
-	}
-	cout <<"Total: "<<allDiscovered.size()<<endl;
-
-	cout << endl << "\n\n===================Game::recolorInPlace test:=================" << endl;
+	cout << endl << "\n\n===================State::recolorInPlace test:=================" << endl;
 	State<3, 4> TestGame3({ 3,1,3,1,2,2,2,2,0,0,0,0 });
 	cout <<"Before:\n";
 	TestGame3.print();
@@ -84,19 +74,20 @@ int main()
 	TestGame4.reColorInPlace();
 	TestGame4.print();
 
-	cout << endl << "\n\n===================Game::discoverAllNonEquivalenStatesFromInitialState test:=================" << endl;
+	cout << endl << "\n\n===================State::discoverAllNonEquivalenStatesFromInitialState test:=================" << endl;
+	const State<4, 4> TestGame2({ 1,2,1,2,1,3,3,2,1,2,3,3,0,0,0,0 });
+	cout << "Initial state:\n";
 	TestGame2.print();
-	allDiscovered = discoverAllNonEquivalenStatesFromInitialState(TestGame2);
+	cout << endl;
+	auto allDiscovered = discoverAllNonEquivalenStatesFromInitialState(TestGame2);
 	for (auto&& s : allDiscovered) {
 		s.print();
 		cout<<endl;
 	}
-	cout <<"Total: "<<allDiscovered.size()<<endl;
+	cout <<"Total #: "<<allDiscovered.size()<<endl;
 
-	std::map<int, double> my_map{ {1,11.0}, {2,22.0} };
+	cout << endl << "\n\n===================Game::factorial test:=================" << endl;
+	cout << "10!=" << factorial(10) <<endl;
 
-
-	//std:array<int, 3> a{ 0,1, 2 };
-	//std:array<int, 3> b{ a, 42 };
 	return 0;
 }
