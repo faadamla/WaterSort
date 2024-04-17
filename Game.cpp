0e29428801f4996a4ff6c413fcab6b20ed51e0a2
faadamla <faadamla@gmail.com>
2024-04-18 01:53:27 +0200

    Implementing initial states. (Unfinished)

--- Game.cpp
+++ Game.cpp
@@ -1,9 +1,5 @@
 #include "State.cpp"
-
-template<uc NumberOfTubes, uc DepthOfTube>
-std::vector<State<NumberOfTubes,  DepthOfTube>> allInitialState(uc ncolors, uc empty=2){
-
-}
+#include<cmath>
 
 long long int factorial(unsigned int n){
     if(n<=1){return 1;}
@@ -14,4 +10,38 @@ long long int factorial(unsigned int n){
         }
         return x;
     }
+}
+
+long long int numberOfInitialStates(uc NumberOfTubes, uc DepthOfTube){
+    return factorial(NumberOfTubes*DepthOfTube)/pow(factorial(DepthOfTube),NumberOfTubes)/factorial(NumberOfTubes);
+}
+
+template<uc NumberOfTubes, uc DepthOfTube>
+std::vector<std::array<uc, NumberOfTubes*DepthOfTube>> generateAllUniqueStates_fillIndex_v1(unsigned int index, const &std::vector<std::array<uc, NumberOfTubes*DepthOfTube>> generatedUntilPreviousIndex){
+    if(index==0){
+        std::array<uc, NumberOfTubes*DepthOfTube> a, b;
+        a.fill(0);
+        b=0;
+        b[0]=1
+        return {a, b}
+    }
+    else{
+        std::vector<std::array<uc, NumberOfTubes*DepthOfTube>> returnvec;
+        uc max_element;
+        std::array<uc, NumberOfTubes*DepthOfTube> arCopy;
+        for(auto&& ar: generatedUntilPreviousIndex){
+            max_element = *(std::max_element(ar.begin(), ar.begin() + i)); //Max element before index i
+            arCopy = ar;
+            for (uc e=1; e<=max_element+1; e++){
+                if (std::count(arCopy.begin(), arCopy.end(), e) < DepthOfTube)
+                arCopy[i] = e;
+                returnvec.emplace_back(arCopy);
+            }
+        }
+    }
+}
+
+template<uc NumberOfTubes, uc DepthOfTube>
+std::vector<State<NumberOfTubes,  DepthOfTube>> allUniqueInitialState(uc empty=2){
+
 }
